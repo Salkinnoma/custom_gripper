372b19c26da0faa1f412b69347ef9695489302a2
Niklas Amon <Niklas.Amon@partner.bmwgroup.com>
2026-01-13 10:17:21 +0100

    Renamed custom_gripper to CustomGripper.cpp

--- components/custom_gripper/__init__.py
+++ components/custom_gripper/__init__.py
@@ -1,14 +1,13 @@
 # __init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
-from esphome import core
-
 from esphome.const import CONF_ID
 
+# This namespace will be referenced in C++
 custom_gripper_ns = cg.esphome_ns.namespace('custom_gripper')
-CustomGripper = custom_gripper_ns.class_('custom_gripper', cg.Component)
+CustomGripper = custom_gripper_ns.class_('CustomGripper', cg.Component)
 
+# This tells ESPHome how to parse the YAML key 'custom_gripper:'
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.declare_id(CustomGripper),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -16,3 +15,4 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+
